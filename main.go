644ef7727394bb2ca84c3153b4a2ffcc997457e0
Jess Frazelle <jess@oxide.computer>
2020-05-14 19:32:02 -0700

    fix
    Signed-off-by: Jess Frazelle <jess@oxide.computer>

--- main.go
+++ main.go
@@ -445,7 +445,7 @@ func updateRelease(ctx context.Context, client *github.Client, repo *github.Repo
 	s := b.String()
 
 	// Check if the body already matches the body we need.
-	if r.Body == &s && r.Name == r.TagName {
+	if *r.Body == s && r.Name == r.TagName {
 		// Return early here.
 		logrus.Debugf("Release is already updated for %s -> %s...", repo.GetFullName(), r.GetTagName())
 		return nil
